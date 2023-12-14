644c105d5ea65c3525e750c391dc2a567825abf5
Kaian Winter de Almeida <kaian.almeida@conlog.local>
2023-12-14 15:13:19 -0300

    Implementacao: Recursos funcionais, correcao erro select filial, correcao erro ordem sortable

--- apps/safety_layout_checklist_app/urls.py
+++ apps/safety_layout_checklist_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('seguranca_check', views.Form_Seguranca_Check.as_view(), name='seguranca_check'),
     path('registra_check', views.Form_Cadastro_Check.as_view(), name='registra_check'),
+    path('filiais_check', views.Form_Filial_Check.as_view(), name='filiais_check'),
+    path('registra_item', views.Form_Item_Check.as_view(), name='registra_item'),
+    path('sortable', views.Sortable_View.as_view(), name='sortable')
 ]
