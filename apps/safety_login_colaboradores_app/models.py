5b23bdb11486b45f560331fcdc84a1577f7a403b
Kaian Winter de Almeida <kaian.almeida@conlog.local>
2024-07-19 14:07:37 -0300

    fix: Arrumado model sem status situação

--- apps/safety_login_colaboradores_app/models.py
+++ apps/safety_login_colaboradores_app/models.py
@@ -12,7 +12,7 @@ class Colaborador(models.Model):
     data_nascimento = models.DateField(blank=True, null=True)
     perfil_usu = models.CharField(max_length=1, blank=False, null=False)
     setor = models.IntegerField(blank=True, null=True)
-
+    situacao = models.IntegerField(blank=True, null=True)
     class Meta:
         managed = True
         db_table = 'op_safe_colaboradores'
