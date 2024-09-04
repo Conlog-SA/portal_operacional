d92b93fcbbf175ae5c5c513f0fa9b83221cc1058
Kaian Winter de Almeida <kaian.almeida@conlog.local>
2024-09-04 09:42:49 -0300

    fix: arrumado erro de model

--- apps/contratos_frete_deep_app/urls.py
+++ apps/contratos_frete_deep_app/urls.py
@@ -4,5 +4,5 @@ from django.views.decorators.csrf import csrf_exempt
 from apps.contratos_frete_deep_app import views
 
 urlpatterns = [
-    path('', csrf_exempt(views.Processa_Contrato.as_view()), name='contratos_frete_deep'),
+    path('contratos_frete_deep', csrf_exempt(views.Processa_Contrato.as_view()), name='contratos_frete_deep'),
 ]
