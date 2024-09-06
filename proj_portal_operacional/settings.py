e8ffc163eaf2fdca0b17bdb2570dc192955281ad
Kaian Winter de Almeida <kaian.almeida@conlog.local>
2024-09-06 09:48:12 -0300

    fix: Trocada versão dos arquivos js, deve ignorar a cache das outros versões

--- proj_portal_operacional/settings.py
+++ proj_portal_operacional/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-VERSAO_SAFETY = '1.6'
+VERSAO_SAFETY = '1.7'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
