6a649b628484f5697f2f957e69f761e720a15f40
Kaian Winter de Almeida <kaian.almeida@conlog.local>
2025-01-21 14:22:04 -0300

    Add: adicionadas novas mudanças em projetos do G&G, dashboard contabil e safety

--- apps/gente_gestao_entrevista_desligamento_app/urls.py
+++ apps/gente_gestao_entrevista_desligamento_app/urls.py
@@ -4,5 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('', csrf_exempt(views.Entrevista_Desligamento.as_view()), name='entrevista_desligamento'),
+    path('', csrf_exempt(views.Entrevista_Desligamento_Gerencial.as_view()), name='entrevista_desligamento'),
+    path('pesquisa_desligamentos', csrf_exempt(views.Frm_Consulta_Entrevista_Desligamento.as_view()), name='pesquisa_desligamentos'),
+    path('entrevista_desligamento', csrf_exempt(views.Frm_Resposta_Entrevista_Desligamento.as_view()), name='entrevista_desligamento'),
 ]
